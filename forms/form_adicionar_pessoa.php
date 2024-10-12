0220517f6556723b2dd5d2fc18bc2f0d556931b3
Cauã Auler <cauaauler@gmail.com>
2024-10-12 14:03:30 -0300

    css

--- forms/form_adicionar_pessoa.php
+++ forms/form_adicionar_pessoa.php
@@ -52,7 +52,7 @@ if (isset($_POST['botao'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Pessoa</title>
-    <link rel="stylesheet" type="text/css" href="/IFRS-Pokepedia/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="/IFRS-Pokepedia/css/pages/login.css" />
 </head>
 <body>
     <div class='container'>
